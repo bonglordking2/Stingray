16962543e194dc91f00db00e3a87cf69958081b2
Nebula-Manta <127610864+Nebula-Manta@users.noreply.github.com>
2025-01-10 18:56:57 +0000

    Update BossFarm.lua

--- JJI/BossFarm.lua
+++ JJI/BossFarm.lua
@@ -5,4 +5,4 @@ until game:IsLoaded()
 
 -- Maintenance --
 setclipboard("https://discord.gg/ja8hEmmX")
-game.Players.LocalPlayer:Kick("Script is in maintenance!!!\n Discord server copied to clipboard for details:\nCheck #important")
+game.Players.LocalPlayer:Kick("Script is in maintenance!!!\n Discord server copied to clipboard for details (updated):\nCheck #important")
