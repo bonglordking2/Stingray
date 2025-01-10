fa58b79fbadb4bbc6161640abe8149cc5fa7f625
Nebula-Manta <127610864+Nebula-Manta@users.noreply.github.com>
2025-01-10 19:47:24 +0000

    Update BossFarm.lua

--- JJI/BossFarm.lua
+++ JJI/BossFarm.lua
@@ -5,12 +5,13 @@ until game:IsLoaded()
 
 -- Maintenance --
 setclipboard("https://discord.gg/ja8hEmmX")
-local function CustomKick(Title, Desc, Button)
-    game.Players.LocalPlayer:Kick()
-    local Prompt = game:GetService("CoreGui"):WaitForChild("RobloxPromptGui"):WaitForChild("promptOverlay")
-    Prompt:FindFirstChild("ErrorTitle",true).Text = Title
-    Prompt:FindFirstChild("ErrorMessage",true).Text = Desc
-    Prompt:FindFirstChild("ButtonText",true).Text = Button
-end
+-- local function CustomKick(Title, Desc, Button)
+--     game.Players.LocalPlayer:Kick()
+--     local Prompt = game:GetService("CoreGui"):WaitForChild("RobloxPromptGui"):WaitForChild("promptOverlay")
+--     Prompt:FindFirstChild("ErrorTitle",true).Text = Title
+--     Prompt:FindFirstChild("ErrorMessage",true).Text = Desc
+--     Prompt:FindFirstChild("ButtonText",true).Text = Button
+-- end
 
-CustomKick("Script Maintenance", "Discord server copied to clipboard for details (invite works now)\n\nCheck #Important for info", "Leave")
+-- CustomKick("Script Maintenance", "Discord server copied to clipboard for details (invite works now)\n\nCheck #Important for info", "Leave")
+game.Players.LocalPlayer:Kick("Script Maintenance\n\nDiscord server copied to clipboard for details (invite works now)\n\nCheck #Important for info")
